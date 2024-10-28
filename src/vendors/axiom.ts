b8c22600f10be4b8d1b8609513b14f7cf1fea6e6
Ironben <hey@ironben.dev>
2024-10-28 11:56:08 +0800

    refactor: axiom can be null

--- src/vendors/axiom.ts
+++ src/vendors/axiom.ts
@@ -1,22 +1,28 @@
 import { Axiom } from '@axiomhq/js';
 import { captureException } from '@sentry/electron/main';
 
-const axiom = new Axiom({
-  token: process.env.AXIOM_TOKEN as string,
-  orgId: process.env.AXIOM_ORG_ID as string,
-});
+let axiom = null;
+
+try {
+  axiom = new Axiom({
+    token: process.env.AXIOM_TOKEN as string,
+    orgId: process.env.AXIOM_ORG_ID as string,
+  });
+} catch (error) {
+  captureException(error);
+}
 
 export default {
   ingest(data: { [key: string]: any }[]) {
     try {
-      axiom.ingest('5ire', data);
+      axiom && axiom.ingest('5ire', data);
     } catch (error) {
       captureException(error);
     }
   },
   async flush() {
     try {
-      await axiom.flush();
+      axiom && (await axiom.flush());
     } catch (error) {
       captureException(error);
     }
