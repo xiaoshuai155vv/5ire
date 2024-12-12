5cc13c06ba1a930ca908342758136d41a217605b
Ironben <hey@ironben.dev>
2024-12-12 12:06:38 +0800

    chore: fix invalid svg attributes

--- src/renderer/components/layout/aside/Footer.tsx
+++ src/renderer/components/layout/aside/Footer.tsx
@@ -90,11 +90,11 @@ export default function Footer({ collapsed }: { collapsed: boolean }) {
                   viewBox="0 0 24 24"
                   fill="none"
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   width="18"
                   height="18"
-                  stroke-width="1.5"
+                  strokeWidth="1.5"
                 >
                   <path d="M4 4l11.733 16h4.267l-11.733 -16z"></path>
                   <path d="M4 20l6.768 -6.768m2.46 -2.46l6.772 -6.772"></path>
@@ -111,11 +111,11 @@ export default function Footer({ collapsed }: { collapsed: boolean }) {
                   viewBox="0 0 24 24"
                   fill="none"
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   width="18"
                   height="18"
-                  stroke-width="1.5"
+                  strokeWidth="1.5"
                 >
                   <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path>
                 </svg>
@@ -131,11 +131,11 @@ export default function Footer({ collapsed }: { collapsed: boolean }) {
                   viewBox="0 0 24 24"
                   fill="none"
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   width="20"
                   height="20"
-                  stroke-width="1.5"
+                  strokeWidth="1.5"
                 >
                   <path d="M12 12c2 -2.96 0 -7 -1 -8c0 3.038 -1.773 4.741 -3 6c-1.226 1.26 -2 3.24 -2 5a6 6 0 1 0 12 0c0 -1.532 -1.056 -3.94 -2 -5c-1.786 3 -2.791 3 -4 2z"></path>
                 </svg>
