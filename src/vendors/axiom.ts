65af9193965a2d3828efc2eba2c3e772aaa45d78
liunnnanbing <exformation@icloud.com>
2024-12-28 11:52:01 +0800

    fix: miss apk token caused init axiom before load env

--- src/vendors/axiom.ts
+++ src/vendors/axiom.ts
@@ -2,20 +2,26 @@
 import { Axiom } from '@axiomhq/js';
 import { captureException } from '../main/logging';
 
-let axiom = null;
+let axiom:any = null;
 
-try {
-  axiom = new Axiom({
-    token: process.env.AXIOM_TOKEN as string,
-    orgId: process.env.AXIOM_ORG_ID as string,
-  });
-} catch (err: any) {
-  captureException(err);
+function getAxiom(){
+  if (!axiom) {
+    try {
+      axiom = new Axiom({
+        token: process.env.AXIOM_TOKEN as string,
+        orgId: process.env.AXIOM_ORG_ID as string,
+      });
+    } catch (err: any) {
+      captureException(err);
+    }
+  }
+  return axiom;
 }
 
 export default {
   ingest(data: { [key: string]: any }[]) {
     try {
+      const axiom = getAxiom();
       axiom && axiom.ingest('5ire', data);
     } catch (err: any) {
       captureException(err);
@@ -23,6 +29,7 @@ export default {
   },
   async flush() {
     try {
+      const axiom = getAxiom();
       axiom && (await axiom.flush());
     } catch (err: any) {
       captureException(err);
