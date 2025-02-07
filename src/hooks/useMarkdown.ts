72376acfb143ba5f13a79d98a468388966b853b1
Ironben <hey@ironben.dev>
2025-02-07 13:53:29 +0800

    ✨: Code blocks support copying
    closes #67

--- src/hooks/useMarkdown.ts
+++ src/hooks/useMarkdown.ts
@@ -1,12 +1,16 @@
 /* eslint-disable react/no-danger */
-
+import { useTranslation } from 'react-i18next';
 // @ts-ignore
 import MarkdownIt from 'markdown-it';
 // @ts-ignore
 import mathjax3 from 'markdown-it-mathjax3'; // @ts-ignore
 import hljs from 'highlight.js/lib/common';
+import MarkdownItCodeCopy from '../libs/markdownit-plugins/CodeCopy';
+import useToast from './useToast';
 
 export default function useMarkdown() {
+  const { notifySuccess } = useToast();
+  const { t } = useTranslation();
   const md = new MarkdownIt({
     html: true,
     linkify: true,
@@ -19,13 +23,17 @@ export default function useMarkdown() {
       if (isLoading) {
         code = str.replace(loader, '');
       }
+
       if (lang && hljs.getLanguage(lang)) {
         try {
           return (
             `<pre className="hljs">` +
-            `<code>${hljs.highlight(lang, code, true).value}${
-              isLoading ? loader : ''
-            }</code></pre>`
+            `<code>${
+              hljs.highlight(code, {
+                language: lang,
+                ignoreIllegals: true,
+              }).value
+            }${isLoading ? loader : ''}</code></pre>`
           );
         } catch (__) {
           return (
@@ -39,13 +47,19 @@ export default function useMarkdown() {
       }
       return (
         `<pre className="hljs">` +
+        `<span class="copy-btn">copy</span>` +
         `<code>${hljs.highlightAuto(code).value}${
           isLoading ? loader : ''
         }</code>` +
         `</pre>`
       );
     },
-  }).use(mathjax3);
+  }).use(mathjax3).use(MarkdownItCodeCopy,{
+    element: '<svg class="___1okpztj f1w7gpdv fez10in fg4l7m0 f16hsg94 fwpfdsa f88nxoq f1e2fz10" fill="currentColor" aria-hidden="true" width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M4 4.09v6.41A2.5 2.5 0 0 0 6.34 13h4.57c-.2.58-.76 1-1.41 1H6a3 3 0 0 1-3-3V5.5c0-.65.42-1.2 1-1.41ZM11.5 2c.83 0 1.5.67 1.5 1.5v7c0 .83-.67 1.5-1.5 1.5h-5A1.5 1.5 0 0 1 5 10.5v-7C5 2.67 5.67 2 6.5 2h5Zm0 1h-5a.5.5 0 0 0-.5.5v7c0 .28.22.5.5.5h5a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.5-.5Z" fill="currentColor"></path></svg>',
+    onSuccess: () => {
+      notifySuccess(t('Common.Notification.Copied'));
+    },
+  });
   const defaultRender =
     md.renderer.rules.link_open ||
     function (tokens: any, idx: any, options: any, env: any, self: any) {
---
+++ src/libs/markdownit-plugins/CodeCopy.ts
@@ -0,0 +1,57 @@
+import merge from 'lodash/merge';
+
+let clipboard:any = null;
+try {
+	// Node js will throw an error
+	this === window;
+
+	const Clipboard = require('clipboard');
+	clipboard = new Clipboard('.markdown-it-code-copy');
+}
+catch (_err) {
+}
+
+const defaultOptions = {
+	iconStyle: 'font-size: 21px; opacity: 0.4;',
+	iconClass: 'mdi mdi-content-copy',
+	buttonStyle: 'position: absolute; top: 7.5px; right: 6px; cursor: pointer; outline: none;',
+	buttonClass: '',
+  element: ''
+};
+
+function renderCode(origRule: (...args: [any, any]) => any, options: { buttonClass: any; buttonStyle: any; iconStyle: any; iconClass: any; element: any; }) {
+	options = merge(defaultOptions, options);
+	return (...args: [any, any]) => {
+		const [tokens, idx] = args;
+		const content = tokens[idx].content
+			.replaceAll('"', '&quot;')
+			.replaceAll("'", "&apos;");
+		const origRendered = origRule(...args);
+		if (content.length === 0)
+			return origRendered;
+
+		return `
+<div style="position: relative">
+	${origRendered}
+	<button class="markdown-it-code-copy ${options.buttonClass}" data-clipboard-text="${content}" style="${options.buttonStyle}" title="Copy">
+		<span style="${options.iconStyle}" class="${options.iconClass}">${options.element}</span>
+	</button>
+</div>
+`;
+	};
+}
+
+export default function MarkdownItCodeCopy(md: any, options: any) {
+	if (clipboard) {
+    clipboard.off('success');
+		if (options.onSuccess) {
+			clipboard.on("success", options.onSuccess);
+		}
+    clipboard.off('onError');
+		if (options.onError) {
+			clipboard.on("error", options.onError);
+		}
+	}
+	md.renderer.rules.code_block = renderCode(md.renderer.rules.code_block, options);
+	md.renderer.rules.fence = renderCode(md.renderer.rules.fence, options);
+};
