1e23250253b36f7321c7b531a58d65d7fe9834b0
Ironben <hey@ironben.dev>
2025-02-18 13:37:02 +0800

    ✨ (useMCPServerMarketStore.ts): add zustand store for MCP server market data with fetch functionality

---
+++ src/stores/useMCPServerMarketStore.ts
@@ -0,0 +1,35 @@
+import { create } from 'zustand';
+import { IMCPServer } from './../types/mcp.d';
+import { captureException } from '../renderer/logging';
+
+const REMOTE_CONFIG_TTL: number = 1000 * 60 * 60 * 24; // 1 day
+
+interface IMCPServerMarketStore {
+  servers: IMCPServer[];
+  updatedAt: number;
+  fetchServers: (force?: boolean) => Promise<IMCPServer[]>;
+}
+
+const useMCPServerMarketStore = create<IMCPServerMarketStore>((set, get) => ({
+  servers: [],
+  updatedAt: 0,
+  fetchServers: async (force = false) => {
+    const { servers, updatedAt } = get();
+    if (!force && updatedAt > Date.now() - REMOTE_CONFIG_TTL) {
+      return servers;
+    }
+    const resp = await fetch('https://statics.5ire.app/mcp/servers.json');
+    if (resp.ok) {
+      const data = await resp.json();
+      set({
+        servers: data,
+        updatedAt: Date.now(),
+      });
+      return data;
+    }
+    captureException(resp.statusText);
+    return [];
+  },
+}));
+
+export default useMCPServerMarketStore;
