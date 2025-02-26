c678dda2fe7647a4e70b8c9f7a048c4a0b03f3a7
nanbingxyz <yunwen@ctbu.edu.cn>
2025-02-26 12:06:09 +0000

    fix style issue on windows

--- src/renderer/components/ToolStatusIndicator.tsx
+++ src/renderer/components/ToolStatusIndicator.tsx
@@ -35,19 +35,19 @@ export default function ToolStatusIndicator(
   const indicator = () => {
     return (
       <div
-        className={`flex justify-center items-center rounded-full border ${originalSupport ? ' border-green-400 dark:border-green-800' : 'border-gray-300 dark:border-neutral-600'}`}
+        className={`flex text-center justify-center items-center rounded-full border ${originalSupport ? ' border-green-400 dark:border-green-800' : 'border-gray-300 dark:border-neutral-600'}`}
         style={{ width: 14, height: 14, borderStyle:'dashed' }}
       >
         {actualSupport ? (
           <div
             className="rounded-full bg-green-400 dark:bg-green-600"
-            style={{ width: 9, height: 9 }}
+            style={{ width: 10, height: 10 }}
             {...rest}
           ></div>
         ) : (
           <div
             className="rounded-full bg-neutral-300  dark:bg-neutral-600"
-            style={{ width: 9, height: 9 }}
+            style={{ width: 10, height: 10 }}
             {...rest}
           ></div>
         )}
