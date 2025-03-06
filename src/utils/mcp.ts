612a46c70404e905ef85ed82335fa01afb7f0f25
Ironben <hey@ironben.dev>
2025-03-06 22:35:41 +0800

    🐛 Fixed an issue where args values were incorrectly replaced when installing tools from the marketplace.
    also update and expand test cases for parameter parsing and substitution
    
    closes #99

--- src/utils/mcp.ts
+++ src/utils/mcp.ts
@@ -2,38 +2,48 @@
  * ['--db-path',<dbPath>] => dbPath
  */
 
-import { flatten, isUndefined } from "lodash";
-import { MCPArgParameter, MCPArgType, MCPEnvParameter, MCPEnvType, IMCPServerParameter } from "types/mcp";
+import { flatten } from 'lodash';
+import {
+  MCPArgParameter,
+  MCPArgType,
+  MCPEnvType,
+  IMCPServerParameter,
+} from 'types/mcp';
 
-export function getParameters(parmas: string[]): IMCPServerParameter[] {
-  const result:IMCPServerParameter[] = []
-  if (!parmas) {
+export function getParameters(params: string[]): IMCPServerParameter[] {
+  const result: IMCPServerParameter[] = [];
+  if (!params) {
     return result;
   }
-  const pattern = /\{\{(?<name>[^@]+)@(?<type>[^:]+)(::(?<description>[^}]*)?)?\}\}/;
-  parmas.forEach((param:string)=>{
+  const pattern =
+    /\{\{(?<name>[^@]+)@(?<type>[^:]+)(::(?<description>[^}]*)?)?\}\}/;
+  params.forEach((param: string) => {
     const match = param.match(pattern);
     if (match && match.groups) {
       result.push({
-            name: match.groups.name,
-            type: match.groups.type as MCPEnvType|MCPArgType,
-            description: match.groups.description||'',
-        }) ;
-  }});
+        name: match.groups.name,
+        type: match.groups.type as MCPEnvType | MCPArgType,
+        description: match.groups.description || '',
+      });
+    }
+  });
   return result;
 }
 
-export function fillArgs(
-  args: string[],
-  params: MCPArgParameter
-): string[] {
-  const pattern = /\{\{(?<name>[^@]+)@(?<type>[^:]+)(::(?<description>[^}]*)?)?\}\}/;
-  let _args:(string|string[])[] = [...args];
-  for (let index=0; index<args.length; index++) {
-    const arg = args[index]
+export function fillArgs(args: string[], params: MCPArgParameter): string[] {
+  const pattern =
+    /\{\{(?<name>[^@]+)@(?<type>[^:]+)(::(?<description>[^}]*)?)?\}\}/;
+  let _args: (string | string[])[] = [...args];
+  for (let index = 0; index < args.length; index++) {
+    const arg = args[index];
     const match = arg.match(pattern);
-    if(match && match.groups){
-      _args[index] = params[match.groups.name]
+    if (match && match.groups) {
+      const paramValue = params[match.groups.name];
+      if (Array.isArray(paramValue)) {
+        _args[index] = paramValue;
+      } else {
+        _args[index] = arg.replace(match[0], paramValue);
+      }
     }
   }
   return flatten(_args);
@@ -41,18 +51,19 @@ export function fillArgs(
 
 export function FillEnv(
   env: Record<string, string> | undefined,
-  params: { [key: string]: string }
+  params: { [key: string]: string },
 ): Record<string, string> {
-  if(!env) return{}
-  const pattern = /\{\{(?<name>[^@]+)@(?<type>[^:]+)(::(?<description>[^}]*)?)?\}\}/;
-  let _env = {...env};
-  const envKeys = Object.keys(env)
-  for(const envKey of envKeys){
-    const envItem = env[envKey]
+  if (!env) return {};
+  const pattern =
+    /\{\{(?<name>[^@]+)@(?<type>[^:]+)(::(?<description>[^}]*)?)?\}\}/;
+  let _env = { ...env };
+  const envKeys = Object.keys(env);
+  for (const envKey of envKeys) {
+    const envItem = env[envKey];
     const match = envItem.match(pattern);
-    if(match && match.groups){
-      _env[envKey] = params[match.groups.name]||''
+    if (match && match.groups) {
+      _env[envKey] = params[match.groups.name] || '';
     }
   }
-  return  _env
+  return _env;
 }
--- test/utils/mcp.test.ts
+++ test/utils/mcp.test.ts
@@ -1,4 +1,4 @@
-import {IMCPArgParameter, IMCPEnvParameter} from '../../src/types/mcp'
+import {MCPArgParameter, MCPEnvParameter} from '../../src/types/mcp';
 import { describe, expect, test } from '@jest/globals';
 import * as mcp from '../../src/utils/mcp';
 
@@ -35,14 +35,21 @@ describe('mcp', () => {
     expect(params4[0].type).toEqual('list');
     expect(params4[0].description).toEqual('');
 
-    const args5 = [''];
+    const args5 = ['--local-timezone={{timezone@string::like Asia/Shanghai. You may need install tzdata first}}'];
     const params5 = mcp.getParameters(args5);
-    expect(params5).toEqual([]);
+    expect(params5.length).toEqual(1);
+    expect(params5[0].name).toEqual('timezone');
+    expect(params5[0].type).toEqual('string');
+    expect(params5[0].description).toEqual('like Asia/Shanghai. You may need install tzdata first');
+
+    const args6 = [''];
+    const params6 = mcp.getParameters(args6);
+    expect(params6).toEqual([]);
 
   });
 
   test('fillArgs', () => {
-    const params = { "dbPath": 'path/to/db', "dbName": '5ire' } as unknown as IMCPArgParameter;
+    const params = { "dbPath": 'path/to/db', "dbName": '5ire' } as unknown as MCPArgParameter;
     const args1 = ['--db-path', '{{dbPath@string::database path}}'];
     const newArgs1 = mcp.fillArgs(args1, params);
     expect(newArgs1).toEqual(['--db-path', 'path/to/db']);
@@ -54,18 +61,24 @@ describe('mcp', () => {
     const params3 = {
       dirs: ['path/1','path/2','path/3'],
       mod: 'write'
-    } as unknown as IMCPArgParameter;
+    } as unknown as MCPArgParameter;
     const args3 = ['--dirs', '{{dirs@list::}}', '--mod', '{{mod@string}}'];
     const newArgs3 = mcp.fillArgs(args3, params3);
     expect(newArgs3).toEqual(['--dirs', 'path/1', 'path/2', 'path/3', '--mod', 'write']);
 
+
     const args4= [''];
     const newArgs4 = mcp.fillArgs(args4, params);
     expect(newArgs4).toEqual(['']);
+
+    const args5 = ['--local-timezone={{timezone@string::like Asia/Shanghai. You may need install tzdata first}}'];
+    const params5 = { "timezone": 'Asia/Shanghai' } as unknown as MCPArgParameter;
+    const newArgs5 = mcp.fillArgs(args5, params5);
+    expect(newArgs5).toEqual(['--local-timezone=Asia/Shanghai']);
   });
 
   test('fillEnv', () => {
-    const params = { "apiKey": '01JMP46SH5M1HD91139N4TCA80', "apiBase": 'https://exapmple.com' } as unknown as IMCPEnvParameter;
+    const params = { "apiKey": '01JMP46SH5M1HD91139N4TCA80', "apiBase": 'https://exapmple.com' } as unknown as MCPEnvParameter;
     const env1 = {
       APIKEY: '{{apiKey@string::API Key}}',
       NAME: 'text',
